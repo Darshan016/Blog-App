643d4db2cd8f3c7c2c67e44b37bd0cb6a35ba926
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-23 13:15:41 +0530

    implemented single post component with single post's info

--- frontend/src/components/singlepost/SinglePost.jsx
+++ frontend/src/components/singlepost/SinglePost.jsx
@@ -2,6 +2,23 @@ import './singlepost.css'
 
 export default function SinglePost() {
   return (
-    <div>SinglePost</div>
+    <div className='singlePost'>
+      <div className="singlePostWrapper">
+        <img src="https://www.universetoday.com/wp-content/uploads/2010/02/Full-Moon.jpg" alt="" className="singlePostImg" />
+        <h1 className="singlePostTitle">Lorem ipsum dolor sapiente.
+        <div className="singlePostEdit">
+        <i class="singlePostIcon fa-regular fa-pen-to-square"></i>
+        <i class="singlePostIcon fa-solid fa-trash"></i></div></h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">Author: darshan</span>
+          <span className="singlePostDate">Date: 1 hr ago</span>
+        </div>
+        <p className='singlePostDesc'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque aspernatur omnis, rerum quod deleniti nobis! Inventore sed voluptatum excepturi dignissimos esse numquam enim, quibusdam voluptates quia? Odit numquam expedita ad?
+        Voluptate praesentium sed vel nemo ab veniam libero magnam tenetur explicabo corporis, ipsa esse! Laudantium fugit, sunt vero aliquid ullam asperiores. Repudiandae magnam adipisci facilis quam minus natus, neque dolore.
+        Rerum cum deserunt esse unde, quam autem, recusandae itaque hic inventore, maxime optio? Voluptas ut laboriosam neque nesciunt modi quia iure temporibus ipsam quibusdam officia? Voluptates quos voluptas ratione ipsum.
+        Sed a ex et deleniti quam placeat dolorum vel iusto vitae, aspernatur tenetur sunt delectus incidunt nostrum! Veniam similique eos voluptatem commodi, tenetur, a sequi consectetur vitae quis expedita eaque.
+        Illo iure hic voluptatum? Quas laboriosam, quam veritatis commodi laborum quisquam ad ab? Qui temporibus vitae nostrum sunt, reiciendis laudantium provident dolore nulla tempora, nihil corporis? Excepturi tempore dicta porro.</p>
+        </div>  
+    </div>
   )
 }
