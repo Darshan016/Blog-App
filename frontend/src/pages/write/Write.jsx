1cd3b81ebbb5c714c4aa9a434aee5c4e34913700
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-23 16:58:33 +0530

    implemented write/publish post with picture upload functionality

--- frontend/src/pages/write/Write.jsx
+++ frontend/src/pages/write/Write.jsx
@@ -3,14 +3,19 @@ import './write.css'
 export default function Write() {
   return (
     <div className='write'>
+        <img src="https://www.universetoday.com/wp-content/uploads/2010/02/Full-Moon.jpg" alt="" className="writeImg" />
         <form className="writeForm">
             <div className="writeFormGroup">
                 <label htmlFor="fileInput">
-                <i class="fa-solid fa-plus"></i>
+                <i class="writeIcon fa-solid fa-plus"></i>
                 </label>
-                <input type="file" id='fileInput' />
-                <input type="text" placeholder='Title' />
+                <input type="file" id='fileInput' style={{display:'none'}} />
+                <input type="text" placeholder='Title' className='writeInput' autoFocus={true} />
             </div>
+            <div className="writeFormGroup">
+                <textarea className="writeInput writeText" placeholder='share your story' type='text'></textarea>
+            </div>
+            <button className="writePublish">Publish</button>
         </form>
 
     </div>
