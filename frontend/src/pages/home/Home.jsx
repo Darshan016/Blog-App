41cad46fa0ffc549adbfda77e7affe1243bb8ad0
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-30 15:36:58 +0530

    rendering post details in home page

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react'
 import Header from '../../components/header/Header'
 import Posts from '../../components/posts/Posts'
 import SideBar from '../../components/sidebar/SideBar'
 import './home.css'
+import axios from 'axios'
+import { useLocation } from 'react-router'
 
 export default function Home() {
+  const [posts, setPosts] = useState([])
+  const {search} = useLocation()
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/api/v1/posts'+search);
+        setPosts(res.data);
+      } catch (error) {
+        console.error('Error fetching posts:', error.message);
+      }
+    };
+    fetchPosts()    
+  },[search])
   return (
     <>
         <Header />
     <div className='home'>
-        <Posts/>
+        <Posts posts={posts} />
         <SideBar/>
     </div>
     </>
