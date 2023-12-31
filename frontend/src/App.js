63c7bff0c2eae1096c4abe54f5ccab01a5f14b5a
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-31 16:18:07 +0530

    used Routes instead of Switch for navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -5,30 +6,26 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Context } from "./context/Context";
 
 function App() {
-  const user = false
+  const {user} = useContext(Context)
   return (
     <Router>
       <TopBar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/register">{user?<Home/>:<Register/>}
-        </Route>
-        <Route path="/login">{user?<Home/>:<Login/>}
-        </Route>
-        <Route path="/write"> {user?<Write/>:<Register/>}
-        </Route>
-        <Route path="/settings">{user?<Settings/>:<Register/>}
-          <Settings />
-        </Route>
-        <Route path="/post/:postId">
-          <Single />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+          
+        <Route path="/register" element={user?<Home/>:<Register/>}/>
+
+        <Route path="/login" element={user?<Home/>:<Login/>}/>
+        <Route path="/write" element= {user?<Write/>:<Register/>}/>
+        <Route path="/settings" element={user?<Settings/>:<Register/>}/>
+
+        <Route path="/post/:postId" element={ <Single />}/>
+         
+      </Routes>
     </Router>
   );
 }
