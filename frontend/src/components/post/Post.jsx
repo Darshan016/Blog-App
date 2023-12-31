bfb4e720c2c2f3b3f5de727dedeb49939a72e86d
Darshan Panchal <panchaldarshan121@gmail.com>
2023-12-31 16:19:15 +0530

    display post image along with post details

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,30 +1,29 @@
-import './post.css'
-import {Link} from 'react-router-dom'
+import "./post.css";
+import { Link } from "react-router-dom";
 
-export default function Post({post}) {
+export default function Post({ post }) {
+  const publicFolder = "http://localhost:3000/images/";
   return (
-    <div className='post'>
-        {post.photo &&(
-            <img className='postImg' src="https://www.waifu.com.mx/wp-content/uploads/2023/05/Zero-Two-3.jpg" alt="" />
-        )}
-        
-        <div className="postInfo">
-            <div className="postCats">
-                {post.categories.map((c)=>(
-                    <span className="postCat">
-                    {c}</span>
+    <div className="post">
+      {post.photo && (
+        <img className="postImg" src={publicFolder + post.photo} alt="" />
+      )}
 
-                ))}
-                
-            </div>
-            <Link to={`/post/${post._id}`} className='link' >
-            <span className="postTitle">{post.title}</span>
-
-            </Link>
-            <hr />
-            <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <p className="postDescription">{post.desc}</p>
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDescription">{post.desc}</p>
     </div>
-  )
+  );
 }
